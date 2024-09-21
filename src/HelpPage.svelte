a7186cfba7cf4beacb06ace0209a5ea5b31c4cc5
Jesse <jesseliot@hotmail.com>
2024-09-21 16:55:38 +0100

    styling

--- src/HelpPage.svelte
+++ src/HelpPage.svelte
@@ -1,33 +1,49 @@
-<script></script>
+<script>
+</script>
 
 <div class="fullCover">
-    <div class="titleContainer">Step 1</div>
-    <div class="imageContainer"></div>
-    <div class="descriptionContainer">Here is how you might want to show the world how to play the game.
-    </div>
+  <div class="stuffContainer">
+    <div class="titleContainer">The Game</div>
+    <div class="imageContainer">game</div>
+    <div class="descriptionContainer">This is how you play lorem ipsum dalets asdkjfh</div>
+  </div>
 </div>
 
 <style>
-    .fullCover {
-        position:absolute;
-        z-index: 1000;
-        background-color: azure;
-        height: 100vh;
-        width: 100vw;
-        display: flex;
-        place-items: center;
-        text-align: center;
-    }
-    .titleContainer {
-        position: relative;
-        display: flex;
-        place-items: center;
-        text-align: right;
-        background-color: burlywood;
-        color: black;
-    }
-    .imageContainer {
-        display: flex;
-        background-image: linear-gradient(red, blue);
-    }
+  .stuffContainer {
+    background-color: deepskyblue;
+    height: 50%;
+  }
+  .fullCover {
+    position: absolute;
+    z-index: 1000;
+    background-color: azure;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .titleContainer {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    background-color: burlywood;
+    color: black;
+    padding:1em 0;
+  }
+  .imageContainer {
+    display: flex;
+    background-color: black;
+    color:aliceblue;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+  .descriptionContainer {
+    justify-content: center;
+    position: relative;
+    color: blue;
+  }
 </style>
