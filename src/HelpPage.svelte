252598514cdd28bcf64e56526ae34f9826c75b28
Jesse <jesseliot@hotmail.com>
2024-09-21 16:35:42 +0100

    starting page

---
+++ src/HelpPage.svelte
@@ -0,0 +1,33 @@
+<script></script>
+
+<div class="fullCover">
+    <div class="titleContainer">Step 1</div>
+    <div class="imageContainer"></div>
+    <div class="descriptionContainer">Here is how you might want to show the world how to play the game.
+    </div>
+</div>
+
+<style>
+    .fullCover {
+        position:absolute;
+        z-index: 1000;
+        background-color: azure;
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        place-items: center;
+        text-align: center;
+    }
+    .titleContainer {
+        position: relative;
+        display: flex;
+        place-items: center;
+        text-align: right;
+        background-color: burlywood;
+        color: black;
+    }
+    .imageContainer {
+        display: flex;
+        background-image: linear-gradient(red, blue);
+    }
+</style>
