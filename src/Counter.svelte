c2136e24a21cd8516fdca4bc41904675e7c1dd61
Jesse <jesseliot@hotmail.com>
2024-09-23 18:20:41 +0100

    Componentised the running count

---
+++ src/Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let fontSize = 0.2;
+    export let size = 1;
+    export let value;
+    export let color;
+</script>
+
+<h1 style:color={color}>{value}</h1>
+<style>
+    div {
+        outline: dotted gold;
+        height:max-content;
+        /* overflow: hidden; */
+        /* display: block;
+        align-items: left;
+        justify-content: left;
+        width: 200px; */
+    }
+    h1{
+        outline: dashed darkmagenta;
+        /* font-size: initial; */
+    }
+</style>
