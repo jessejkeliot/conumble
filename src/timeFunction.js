6048d2ada085b71303896e07d9273b5d13284aaa
Jesse <jesseliot@hotmail.com>
2024-10-08 19:17:30 +0100

    date fix

--- src/timeFunction.js
+++ src/timeFunction.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 export function getTimeFromFirstConumble() {
   const today = new Date();
-  const startDate = new Date("2024-10-8");
+  const startDate = new Date("2024-10-08");
   const timeDiff = today.valueOf() - startDate.valueOf(); //in milliseconds
   return timeDiff;
 }
